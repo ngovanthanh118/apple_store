764eb8b54c392997f75b4b1932c3121c830a7652
ngovanthanh118 <nvthanh118@gmail.com>
2023-10-28 22:31:44 +0700

    test

--- src/components/NewProducts/index.js
+++ src/components/NewProducts/index.js
@@ -12,7 +12,7 @@ export default function NewProducts() {
         window.scroll(0, 0);
     }, []);
     return (
-        <div className="p-8 bg-gray-200">
+        <div className="p-8 bg-gray-200 h-screen">
             <h1 className="text-black font-normal text-4xl my-2">New Products</h1>
             <div className="grid grid-cols-4 gap-5 p-4 w-full ">
                 {data.slice(0, 8).map(product => (
