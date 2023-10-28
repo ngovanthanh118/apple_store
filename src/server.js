a416dc462059cea5c46c06e3bbf1183f24187870
ngovanthanh118 <nvthanh118@gmail.com>
2023-10-28 22:37:37 +0700

    fix json-server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('../data/db.json')
-const middlewares = jsonServer.defaults({
-  static: "./build",
-})
-server.use(middlewares)
-server.use(jsonServer.rewriter({
-  "/api/*":  "/$1",
-}))
-server.use(router)
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running ${PORT}`)
-})
+const jsonServer = require('json-server');
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults();
+const router = jsonServer.router('../data/db.json');
+const port = process.env.PORT || 4000;
+
+app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+server.listen(port);
